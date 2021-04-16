be2873142833ded94380694e25c814963b13fc86
danmonteirosilva <danillomonteiro182@gmail.com>
2021-04-15 22:38:44 -0300

    classde caixa, precisa corridir

--- principal/models.py
+++ principal/models.py
@@ -11,4 +11,21 @@ class Balconista(models.Model):
     telefone = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     senha = models.CharField(max_length=100)
+
+
+class Caixa(models.Model):
+    codigo_caixa: models.DecimalField(max_digits=10)
+    qtd: models.DecimalField(max_digits=10)
+    valor_unit: models.DecimalField(max_digits=7, decimal_places=2)
+    valot_total: models.DecimalField(max_digits=7, decimal_places=2)
+    taxa_entrega: models.DecimalField(max_digits=7, decimal_places=2)
+    forma_pagamento: models.CharField(max_length=100)
+    convenio: models.BooleanField(default=False)
+    dinheiro: models.DecimalField(max_digits=7, decimal_places=3)
+    troco: models.DecimalField(max_digits=7, decimal_places=3)
+    vendedor: models.ForeignKey(Balconista, on_delete=models.CASCADE)
+    observacoes: models.TextField()
+
+
+
 # Create your models here.
