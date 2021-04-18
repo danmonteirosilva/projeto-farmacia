e3cd64e17af25684c11f6b3f11a7867f833c3f98
danmonteirosilva <danillomonteiro182@gmail.com>
2021-04-18 16:47:47 -0300

    resolvendo conflito admin

--- principal/admin.py
+++ principal/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
-from .models import Balconista
-<<<<<<< HEAD
-from .models import Caixa
-=======
+from .models import Balconista,Farmaceutico
 
 
 admin.site.register(Balconista)
->>>>>>> Robertokalleb-master
-
-admin.site.register(Balconista)
-admin.site.register(Caixa)
-# Register your models here.
+admin.site.register(Farmaceutico)
