bc8337f78e5561374d7716347d790934069e3a90
Robertokalleb <64927176+Robertokalleb@users.noreply.github.com>
2021-04-18 18:04:31 -0300

    criado a balconistalistView e listaView.html

--- principal/admin.py
+++ principal/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Balconista,Farmaceutico
-
+from .models import Balconista, Farmaceutico, Cliente
 
 admin.site.register(Balconista)
 admin.site.register(Farmaceutico)
-
-
+admin.site.register(Cliente)
--- principal/views.py
+++ principal/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from .models import Balconista,Farmaceutico
+from .models import Balconista, Farmaceutico
 from django.views.generic import CreateView
+from django.views.generic import ListView
 from django.urls import reverse_lazy
+
 
 class BalconistaCreateView(CreateView):
     model = Balconista
@@ -12,12 +14,17 @@ class BalconistaCreateView(CreateView):
     def get_success_url(self):
         return reverse_lazy('Balconista')
 
+
 class FarmaceuticoCreateView(CreateView):
     model = Farmaceutico
     template_name = 'farmaceutico.html'
 
     fields = '__all__'
 
-
     def get_success_url(self):
         return reverse_lazy('Farmaceutico')
+
+
+class BalconistaListView(ListView):
+    model = Balconista
+    template_name = 'listar/balconista'
