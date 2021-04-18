5792e95fa212439f55266a12b679c2abadbd7eeb
danmonteirosilva <danillomonteiro182@gmail.com>
2021-04-18 19:25:42 -0300

    adicionando as classes de produto e venda

--- principal/admin.py
+++ principal/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Balconista,Farmaceutico
+from .models import Balconista,Farmaceutico, Cliente, Produto, Venda
 
 
 admin.site.register(Balconista)
 admin.site.register(Farmaceutico)
+admin.site.register(Cliente)
+admin.site.register(Produto)
+admin.site.register(Venda)
