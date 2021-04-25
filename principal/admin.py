6ce908efff7db47e04e7e7f9c0ebeb8788d4be8a
danmonteirosilva <danillomonteiro182@gmail.com>
2021-04-25 20:40:22 -0300

    criando as classes de parceiros e repositorio de contratos

--- principal/admin.py
+++ principal/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Balconista,Farmaceutico, Cliente, Produto, Venda, Receita, Fornecedor, Compra, Pagamento, Contas_a_pagar, Entregador
+from .models import Balconista,Farmaceutico, Cliente, Produto, Venda, Receita, Fornecedor, Compra, Pagamento, Contas_a_pagar, Entregador, Parceiro, Repositorio_contrato
 
 
 admin.site.register(Balconista)
@@ -13,3 +13,5 @@ admin.site.register(Compra)
 admin.site.register(Pagamento)
 admin.site.register(Contas_a_pagar)
 admin.site.register(Entregador)
+admin.site.register(Parceiro)
+admin.site.register(Repositorio_contrato)
