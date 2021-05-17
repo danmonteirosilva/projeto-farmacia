25ae6149e54ff9722e9f8d9d0e211957bc71fc32
RobertoKalleb <64927176+Robertokalleb@users.noreply.github.com>
2021-05-17 15:56:09 -0300

    adiocionando template  de balconista e farmaceutico

--- principal/urls.py
+++ principal/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import BalconistaCreateView
-from .views import BalconistaListView
+from .views import BalconistaCreateView,FarmaceuticoCreateView
+from .views import BalconistaListView,FarmaceuticoListView
 
 urlpatterns = [
-    path('cadastrar/balconista', BalconistaCreateView.as_view(), name='cadastrar_balconista'),
-    path('listar/balconista', BalconistaListView.as_view(), name='listar_balconista'),
+    path('cadastrar/balconista', BalconistaCreateView.as_view(), name ='cadastrar_balconista'),
+    path('cadastrar/farmaceutico', FarmaceuticoCreateView.as_view(), name ='cadastrar_farmaceutico'),
+    path('listar/balconista', BalconistaListView.as_view(), name ='listar_balconista'),
+    path('listar/farmaceutico', FarmaceuticoListView.as_view(), name ='listar_farmaceutico'),
 
 
 ]
--- principal/views.py
+++ principal/views.py
@@ -1,7 +1,5 @@
 from .models import Balconista, Farmaceutico, Cliente, Produto
-from django.views.generic import CreateView
-from django.views.generic import ListView
-from django.views.generic import UpdateView
+from django.views.generic import CreateView, ListView, UpdateView
 from django.urls import reverse_lazy
 
 
@@ -14,10 +12,24 @@ class BalconistaCreateView(CreateView):
     def get_success_url(self):
         return reverse_lazy('cadastrar_balconista')
 
-
+class FarmaceuticoCreateView(CreateView):
+    model =  Farmaceutico
+    template_name ='cadastrar/farmaceutico.html'
+    
+    fields = '__all__'
+    
+    def get_success_url(self):
+        return reverse_lazy('cadastrar_farmaceutico')
+    
+    
 class BalconistaListView(ListView):
     model = Balconista
     template_name = 'listar/balconista.html'
+    paginate_by = 3
+
+class FarmaceuticoListView(ListView):
+    model = Farmaceutico
+    template_name = 'listar/farmaceutico.html'
     paginate_by = 3
 
 
